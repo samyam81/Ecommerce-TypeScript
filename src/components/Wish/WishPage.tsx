f6ced9572e770c88ee50fb878f8cadba8b0be8d1
samyam81 <samyam081@gmail.com>
2024-12-29 17:10:16 +0545

    typo.

--- src/components/Wish/WishPage.tsx
+++ src/components/Wish/WishPage.tsx
@@ -31,7 +31,7 @@ const Wishpage = () => {
                   width: "20rem",
                   transition: "transform 0.3s ease, box-shadow 0.3s ease",
                   opacity: 0,
-                  Animation: "fadeIn 0.5s forwards",
+                  animation: "fadeIn 0.5s forwards",
                 }}
                 onMouseEnter={(e) => {
                   e.currentTarget.style.transform = "scale(1.05)";
